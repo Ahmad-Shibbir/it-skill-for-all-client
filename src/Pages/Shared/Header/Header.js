3c6e86c0d8bf73ac282e1414a374ad245cf42ba7
Ahmad-Shibbir <ahmadshibbir.iium@gmail.com>
2022-10-25 05:20:28 +0800

    nav bar added

--- src/Pages/Shared/Header/Header.js
+++ src/Pages/Shared/Header/Header.js
@@ -1,10 +1,40 @@
+import Container from 'react-bootstrap/Container';
+import Nav from 'react-bootstrap/Nav';
+import Navbar from 'react-bootstrap/Navbar';
+import { Link } from 'react-router-dom';
+import logo from '../../../assets/logo.ico'
 
 
 const Navigation = () => {
     return (
         <div>
-            <h1>This is navigation page</h1>
+            <Navbar collapseOnSelect expand="lg" bg="light" variant="light">
+                <Container>
+                    <img src={logo} alt="" width="60" height="60" />
+                    <Navbar.Brand href="#home">  ITskills4all</Navbar.Brand>
+                    <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+                    <Navbar.Collapse id="responsive-navbar-nav">
+                        <Nav className="me-auto">
+                            <Nav.Link ><Link to='/home'>Home</Link></Nav.Link>
+                            <Nav.Link ><Link to='/courses-container'>Courses</Link></Nav.Link>
+                            <Nav.Link ><Link to='/blog'>Blog</Link></Nav.Link>
+                            <Nav.Link ><Link to='/faq'>FAQ</Link></Nav.Link>
+                            {/* <Nav.Link href="#pricing">Pricing</Nav.Link> */}
+
+                        </Nav>
+                        <Nav>
+                            <Nav.Link href="#deets">user info</Nav.Link>
+                            <Nav.Link eventKey={2} href="#memes">
+                                email
+                            </Nav.Link>
+                        </Nav>
+                    </Navbar.Collapse>
+                </Container>
+            </Navbar>
+
+            
         </div>
+
     );
 };
 
